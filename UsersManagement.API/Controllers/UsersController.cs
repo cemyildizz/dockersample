6ace12c5c5bf63aa4f68e80af0a08e31b0380661
cemyildiz95 <30651908+cemyildiz95@users.noreply.github.com>
2020-05-08 04:25:33 +0300

    Add project files.

---
+++ UsersManagement.API/Controllers/UsersController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UsersManagement.API.Fake;
+using UsersManagement.API.Models;
+
+namespace UsersManagement.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class UsersController:ControllerBase
+    {
+        private List<Users> _users = FakeData.GetUsers(20);
+
+        [HttpGet]
+        public List<Users> Get()
+        {
+            return _users;
+        }
+
+        [HttpGet("{id}")]
+        public Users Get(int id)
+        {
+            var user = _users.FirstOrDefault(x => x.Id == id);
+            return user;
+        }
+
+        [HttpPost]
+        public Users Post([FromBody]Users user)
+        {
+            _users.Add(user);
+            return user;
+        }
+
+        [HttpPut]
+        public Users Put([FromBody]Users user)
+        {
+            var updateUser = _users.FirstOrDefault(x => x.Id == user.Id);
+            updateUser.FirstName = user.FirstName;
+            updateUser.LastName = user.LastName;
+            updateUser.Address = user.Address;
+            return user;
+        }
+
+        [HttpDelete]
+        public void Delete(int id)
+        {
+            var deletedUser = _users.FirstOrDefault(x => x.Id == id);
+            _users.Remove(deletedUser);
+        }
+    }
+}
---
+++ UsersManagement.API/Fake/FakeData.cs
@@ -0,0 +1,29 @@
+﻿using Bogus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UsersManagement.API.Models;
+
+namespace UsersManagement.API.Fake
+{
+    public static class FakeData
+    {
+        private static List<Users> _users;
+
+        public static List<Users> GetUsers(int number)
+        {
+            if (_users==null)
+            {
+                _users = new Faker<Users>()
+                .RuleFor(x => x.Id, f => f.IndexFaker + 1)
+                .RuleFor(x => x.FirstName, f => f.Name.FirstName())
+                .RuleFor(x => x.LastName, f => f.Name.LastName())
+                .RuleFor(x => x.Address, f => f.Address.FullAddress())
+                .Generate(number);
+            }
+
+            return _users;
+        }
+    }
+}
